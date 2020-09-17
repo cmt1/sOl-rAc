44de5e20eae492ad55e8e4017482bef62bd01c98
Carlos Torres <32316110+cmt1@users.noreply.github.com>
2020-09-17 00:46:15 -0700

    updated files

--- docs/unoPunch.js
+++ docs/unoPunch.js
@@ -3,11 +3,15 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    parent: 'content',
     physics: {
         default: 'arcade',
         arcade: {
     
         }
+    },
+     dom: {
+        createContainer: true
     },
     //This is setting up the different levels
     scene: [ world1 ]
