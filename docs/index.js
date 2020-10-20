f23322c67eac21af7553f764901479200a14545f
Carlos Torres <32316110+cmt1@users.noreply.github.com>
2020-10-19 22:45:34 -0700

    update

---
+++ docs/index.js
@@ -0,0 +1,52 @@
+function right() {
+    const r = document.getElementById('mainc');
+    r.style.left = `${r.offsetLeft + 30}px`;
+}
+
+
+function left() {
+    const l = document.getElementById('mainc');
+    l.style.left = `${l.offsetLeft - 30}px`;
+}
+
+function up() {
+    const l = document.getElementById('mainc');
+    l.style.top = `${l.offsetTop - 50}px`;
+}
+
+function down() {
+    const l = document.getElementById('mainc');
+    l.style.top = `${l.offsetTop + 30}px`;
+}
+
+function move() {
+    var mover = document.getElementById('right');
+   
+    if (mover.style.display === "none") {
+        mover.style.display = "block";
+
+
+    }
+  
+}
+function mover() {
+   
+    var stand = document.getElementById('mainc');
+
+    if (stand.style.display === "block") {
+        stand.style.display = "none";
+
+
+    }
+   
+}
+
+
+ 
+function punchs() {
+   var img1 = document.getElementById('punch');
+  if (img1.style.display === "none") {
+        img1.style.display = "block";
+    }
+}
+
