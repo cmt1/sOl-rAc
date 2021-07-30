af825671b587d22768b9a4b1207cc295c25665d8
Carlos Torres <32316110+cmt1@users.noreply.github.com>
2021-07-30 03:23:11 -0700

    update

--- docs/index.js
+++ docs/index.js
@@ -4,42 +4,4 @@ function right() {
 }
 
 
-function left() {
-    const l = document.getElementById('mainc');
-    l.style.left = `${l.offsetLeft - 30}px`;
-}
 
-function up() {
-    const l = document.getElementById('mainc');
-    l.style.top = `${l.offsetTop - 50}px`;
-}
-
-function down() {
-    const l = document.getElementById('mainc');
-    l.style.top = `${l.offsetTop + 30}px`;
-}
-
-function move() {
-    var mover = document.getElementById('right');
-   
-    if (mover.style.display === "none") {
-        mover.style.display = "block";
-
-
-    }
-  
-}
-function mover() {
-   
-    var stand = document.getElementById('mainc');
-
-    if (stand.style.display === "block") {
-        stand.style.display = "none";
-
-
-    }
-   
-}
-
-
- 
